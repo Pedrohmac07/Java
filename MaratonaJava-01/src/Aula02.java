ddf1a9538ea275eb56769e707979d00cda5e9d7a
Pedro Machado <ph374011@gmail.com>
2025-10-20 21:08:37 -0300

    idk what happened lol

---
+++ MaratonaJava-01/src/Aula02.java
@@ -0,0 +1,13 @@
+public class Aula02 {
+    public static void main(String[] args) {
+        int age = 18;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 18;
+        boolean legalAge = true;
+        char character = 'W';
+
+        System.out.println("Age : " + age);
+    }
+}
