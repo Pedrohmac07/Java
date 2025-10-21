d7f941a60640406b218a6dd571dbe17f456c4312
Pedro Machado <ph374011@gmail.com>
2025-10-20 21:14:58 -0300

    Added Aula03

---
+++ MaratonaJava-01/src/Aula03.java
@@ -0,0 +1,16 @@
+public class Aula03 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        int sub = num1 - num2;
+        float div = num1 / num2;
+        int mul = num1 * num2;
+
+        System.out.println("sum =" + sum);
+        System.out.println("subtract =" + sub);
+        System.out.println("div =" + div);
+        System.out.println("mul =" + mul);
+    }
+}
